d821abd63ba624297dc66e0842a3132d8c51353c
juven1996 <juven1996@gmail.com>
2016-05-03 18:22:11 -0700

    changed size and background

--- src/projectprototype/Game.java
+++ src/projectprototype/Game.java
@@ -15,53 +15,53 @@ import javax.swing.border.Border;
  * @author juven1996
  */
 public class Game {
-    protected static int Width = 640;
-    protected static int Height = 480;
+
+    protected static int Width = 1280;
+    protected static int Height = 1040;
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         JFrame frame = new JFrame("Prototype Game");
-        
+        JPanel gamePanel = new JPanel(new BorderLayout());
+        Border border = BorderFactory.createEtchedBorder();
         JMenuBar menuBar = new JMenuBar();
-        
         JMenu gameMenu = new JMenu("Game");
-        gameMenu.setMnemonic(KeyEvent.VK_G);
-        
         JMenuItem newGame = new JMenuItem("New Game");
-        newGame.setMnemonic(KeyEvent.VK_N);
-        
         JMenuItem exit = new JMenuItem("Exit");
-        exit.setMnemonic(KeyEvent.VK_E);    
-        
+
+        gameMenu.setMnemonic(KeyEvent.VK_G);
+        newGame.setMnemonic(KeyEvent.VK_N);
+        exit.setMnemonic(KeyEvent.VK_E);
+
         newGame.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent event) {
-                 JOptionPane.showMessageDialog(frame,
-                    "New Game Created.", "Game Message", JOptionPane.PLAIN_MESSAGE);
+                JOptionPane.showMessageDialog(frame,
+                        "New Game Created.", "Game Message", JOptionPane.PLAIN_MESSAGE);
             }
         });
-        
+
         exit.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent event) {
-            System.exit(0);
+                System.exit(0);
             }
         });
-        
+
         gameMenu.add(newGame);
         gameMenu.add(exit);
         menuBar.add(gameMenu);
         frame.setJMenuBar(menuBar);
-        
+
         frame.setBounds(0, 0, Width, Height);
-        frame.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE );
-        JPanel contentPane = new JPanel( new BorderLayout() );
-        Border border = BorderFactory.createEtchedBorder();
-        border = BorderFactory.createTitledBorder( border, "Game Panel will be below");
-        contentPane.setBorder( border );
-        frame.setContentPane( contentPane );
+        frame.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        border = BorderFactory.createTitledBorder(border, "Game Panel will be below");
+        gamePanel.setBorder(border);
+        gamePanel.setBackground(Color.WHITE);
+        frame.setContentPane(gamePanel);
         frame.setVisible(true);
     }
-    
+
 }
