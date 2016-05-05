e2c56c59593cd7d2672d16a92d0b7b25a15b3a3a
juven1996 <juven1996@gmail.com>
2016-05-05 14:34:39 -0700

    fixed udp client

--- src/projectprototype/SocketTest.java
+++ src/projectprototype/SocketTest.java
@@ -14,10 +14,10 @@ import java.net.*;
  */
 public class SocketTest {
 
-    int port = 4242;
-    String host = "127.0.0.2";
-    
-    public void testAvailablility() throws IOException {
+    static int port = 4242;
+    static String host = "127.0.0.2";
+
+    public static void testAvailablility() throws IOException {
         Socket gp3Client = new Socket(host, port);
         OutputStream dataFeed = gp3Client.getOutputStream();
         OutputStreamWriter dataWrite = new OutputStreamWriter(dataFeed);
@@ -35,27 +35,16 @@ public class SocketTest {
             InetAddress IP = InetAddress.getLocalHost();
             InetSocketAddress groupEP = new InetSocketAddress(IP.getHostAddress(), 4244);
             DatagramSocket udpClient = new DatagramSocket(4244);
-            udpClient.bind(groupEP);
             udpClient.setReuseAddress(true);
             dataWrite.write("<SET ID=\"IMAGE_TX\" ADDRESS=\"127.0.0.1\" PORT=\"4244\" />\r\n");
             dataWrite.flush();
-            dataWrite.write("<SET ID=\"ENABLE_SEND_IMAGE\" STAGE=\"1\" />\r\n");
-            dataWrite.flush();
-            byte[] buffer = new byte[4096];
-            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
             System.out.println("UDP client created");
         } catch (Exception e) {
             System.out.println("Failed to create UDP Client");
         }
-            dataWrite.write("<SET ID=\"ENABLE_SEND_TIME\" STATE=\"1\" />\r\n");
-            dataWrite.write("<SET ID=\"ENABLE_SEND_POG_FIX\" STATE=\"1\" />\r\n");
-            dataWrite.write("<SET ID=\"ENABLE_SEND_CURSOR\" STATE=\"1\" />\r\n");
-            dataWrite.write("<SET ID=\"ENABLE_SEND_DATA\" STATE=\"1\" />\r\n");
-            
     }
 
     public static void main(String[] args) throws SocketException, UnknownHostException, IOException {
-        SocketTest st = new SocketTest();
-        st.testAvailablility();
+        testAvailablility();
     }
 }
