e860cd66ab456a9768b11b83a3e071f90f17356e
unknown <megaman22342@live.ca>
2016-05-22 20:02:54 -0700

    Fixed network

--- src/projectprototype/Server.java
+++ src/projectprototype/Server.java
@@ -166,8 +166,7 @@ public class Server implements Serializable {
                             }
                         }
                         if (!found) {
-                            circle = new Circle(circle, Color.blue);
-                            circle.player = panel.player1;
+                            circle = new Circle(circle, panel.player1, Color.blue);
                             panel.player1.objects.add(circle);
                         }
                         found = false;
